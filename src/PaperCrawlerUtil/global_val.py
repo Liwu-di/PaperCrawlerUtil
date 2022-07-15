04e980b47df8aa50637877d2b76a4ad3c54b7653
Liwu-di <liwudi@liwudi.fun>
2022-07-15 20:52:25 +0800

    全局变量模块

--- src/PaperCrawlerUtil/global_val.py
+++ src/PaperCrawlerUtil/global_val.py
@@ -4,4 +4,19 @@
 # @FileName: global_val.py
 # @Software: PyCharm
 # @Email   ：liwudi@liwudi.fun
+def _init():  # 初始化
+    global _global_dict
+    _global_dict = {}
 
+
+def set_value(key, value):
+    """定义一个全局变量"""
+    _global_dict[key] = value
+
+
+def get_value(key):
+    """获得一个全局变量，不存在则提示读取对应变量失败"""
+    try:
+        return _global_dict[key]
+    except Exception as e:
+        print('读取'+key+'失败:{}\r\n'.format(e))
