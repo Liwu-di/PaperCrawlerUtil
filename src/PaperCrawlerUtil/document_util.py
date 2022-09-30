ee62ad5b4a63138bd03865c6ca6fd76ec4a42147
Liwu-di <liwudi@liwudi.fun>
2022-09-30 15:49:02 +0800

    取消测试代码

--- src/PaperCrawlerUtil/document_util.py
+++ src/PaperCrawlerUtil/document_util.py
@@ -2,6 +2,9 @@ from translate_util import *
 from pdf_util import *
 from office_util import *
 
-
 if __name__ == "__main__":
     basic_config(logs_style=LOG_STYLE_PRINT)
+    # for i in range(7):
+    #     getSomePagesFromFileOrDirectory(
+    #         path="D:\\python project\\PaperCrawlerUtil\\src\\PaperCrawlerUtil\\09_43_0370543.pdf",
+    #         page_range=(i * 50, (i + 1) * 50))
